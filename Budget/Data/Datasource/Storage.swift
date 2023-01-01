f355cb122e201677c106353c1f9d143aa91abcf0
Rafael Escaleira <rafael.escaleira@g.globo>
2023-01-01 16:18:33 -0400

    Create storage to center all managers local data

---
+++ Budget/Data/Datasource/Storage.swift
@@ -0,0 +1,14 @@
+//
+//  Storage.swift
+//  Budget
+//
+//  Created by Rafael Santos on 30/12/22.
+//
+
+import SwiftUI
+
+final class Storage {
+    static let shared = Storage()
+    let category: CategoryStorage = .shared
+    let transaction: TransactionStorage = .shared
+}
